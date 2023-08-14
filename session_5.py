710760d12fbe8e6c2325cef96c54887cd31bc551
Om Bhoge <bhogeom28@gmail.com>
2023-08-14 15:35:27 +0530

    Commit

--- session_5.py
+++ session_5.py
@@ -1,8 +1,78 @@
 # Operatios In Python 
 # 1) Arithmetic operatrs +,-,*,/,//,%,**
+# 2) Comparison operators < , > , <= , >= , == , !=
+# 3) Identity operators is , is not
+# 4) Assignment operators = , += , -= , *= , /=
+# 5) Bitwise operators & , | , ^ , >> , <<
+# 6) Logical operators and , or , not
+# 7) Membership operators in , not in
 
-a = 10
-b = 20
-print(a+b)
-print(a-b)
-print(a*b)
+# 1)
+
+# a = 10
+# b = 20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+# print(10/3)
+# print(10//3)
+# print(10%3)
+
+# print(10**2)
+
+# 2) 
+
+# num1 = 100
+# num2 = 50
+
+# print(num1 < num2)
+# print(num1 > num2)
+# print(num1 <= num2)
+# print(num1 >= num2)
+# print(num1 == num2)
+# print(num1 != num2)
+# print(num1 is num2)
+
+# 3)
+
+# l = [10,20,30]
+# l2 = [10,20,30]
+
+# print(l == l2)
+# print(l is l2)
+# print(l is not l2)
+
+# 4)
+
+# num1 = 100
+# num1 = num1 + 5
+# print(num1)
+# num1+=5
+# print(num1)
+
+# 5)
+
+# num1 = 2
+# num2 = 1
+# print(bin(num1),bin(num2))
+# print(num1 & num2)
+# print(num1 | num2)
+# print(num1 >> num2)
+# print(num1 << num2)
+
+# 6)
+
+# print(10==15 and 20==20)
+# print(10==15 or 20==20)
+# print(not(10==10))
+
+# 7)
+
+# l = [10,20,30,40,50]
+# print(30 in l)
+# print(300 in l)
+# print(50 not in l)
+
+# s = "Python String"
+# print("Python" not in s)
