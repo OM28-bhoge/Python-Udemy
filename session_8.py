743f70bf211cb3006a39985b52d2b2e5597e48cc
Om Bhoge <bhogeom28@gmail.com>
2023-08-15 17:31:21 +0530

    commit

--- session_8.py
+++ session_8.py
@@ -8,14 +8,34 @@
 l = [10,20,30,40,50,60]
 key = 300
 
-for index,value in enumerate(l):
-    print(index, value)
+# for index,value in enumerate(l):
+#     # print(index, value)
 #     if value == key:
-#         print("Element Found")
+#         print("Element Found at index" , index)
 #         break
 #     else:
 #         continue
+#         print("statement after continue statement")
 # else:
 #     print("Element Not Found")
 
 # print("Statemnt after the for loop")
+
+for index,value in enumerate(l):
+    # print(index, value)
+    if value == key:
+        print("Element Found at index" , index)
+        break
+    else:
+        pass
+        # print("statement after continue statement")
+else:
+    print("Element Not Found")
+
+# for value in l:
+
+# else
+# break
+# continue
+# pass
+# enumerate
