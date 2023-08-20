f372976f98a63d1d268ad87a2a7bf66d0a468603
Om Bhoge <bhogeom28@gmail.com>
2023-08-20 13:45:08 +0530

    Commit

---
+++ session_12.py
@@ -0,0 +1,84 @@
+# # help(str)
+# # print(dir(str))
+# # format
+# num1 = 100
+# num2 = 200
+# print("Value of num1 is {val2} value of num2 is {val1}".format(val1=num1, val2=num2))
+
+# s = "Python Sample String"
+# print(id(s))
+# s1 = s.capitalize()
+# s = s.lower()
+# print(s.islower())
+# print(id(s))
+
+# upper
+# lower
+# title
+
+# print(s.upper())
+# print(s.lower())
+# print(s.title())
+
+# isupper
+# islower
+# istitle
+# iscapitalize
+
+# split
+# join
+
+# s = "HTML , CSS ,PYTHON ,JAVA ,Django"
+# l = s.split("  ")
+# print(l)
+
+# s1 = (" ").join(l)
+# print(s1)
+
+# s1 = "abcd"
+# s2 = "1234"
+
+# s3 = "Python sample String abcd"
+
+# table = str.maketrans(s1, s2)
+# result = s3.translate(table)
+# print(result)
+# maketrans
+# translate
+
+
+# Index
+# find
+# rfind function
+
+# s = "HTML,CSS,PYTHON,PYTHON,PYTHON"
+# print("PYTHON" in s)
+# print(s.index("PYTHON"))
+
+# print(s.find("PYTHON"))
+# print(s.find("python"))
+
+# print(s.rfind("PYTHON"))
+
+# s = "      This is sample string     "
+# s = "******This is sample string******"
+# s1 = s.strip(" ")
+# s1 = s.strip("*")
+# s1 = s.lstrip("*")
+# s1 = s.rstrip("*")
+# print(s1)
+
+# s = "python"
+
+# s1 = s.center(20,"*")
+# s1 = s.ljust(20,"*")
+# s1 = s.rjust(20,"*")
+# print(s1)
+
+# s = "html,css,python,html"
+
+# s1 = s.replace("html","HTML5")
+# print(s1)
+
+# help(str)
+# print(dir(str))
